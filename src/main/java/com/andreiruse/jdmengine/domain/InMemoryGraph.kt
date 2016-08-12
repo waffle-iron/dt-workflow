05ee97c6103c0c55821d56ccf56e5cdf9034ce1a
Andrei Ruse <andreiruse@users.noreply.github.com>
2016-08-12 23:54:17 +0100

    Create in memory graph from bpmn (#7)
    * Added dependency on org.jgrapht and built an in-memory graph corresponding to the BPMN input.
    Not tested
    
    * Added sample BPMN to be used for test runs

---
+++ src/main/java/com/andreiruse/jdmengine/domain/InMemoryGraph.kt
@@ -0,0 +1,50 @@
+package com.andreiruse.jdmengine.domain
+
+import org.jgrapht.DirectedGraph
+import org.jgrapht.graph.DefaultDirectedGraph
+import org.jgrapht.graph.DefaultEdge
+import org.w3c.dom.Node
+import java.util.*
+
+class InMemoryGraph {
+    private var graphNodes: MutableMap<String, Pair<String, String>> = HashMap()
+    private var _graph: DirectedGraph<Pair<String, String>, DefaultEdge> = DefaultDirectedGraph(DefaultEdge::class.java)
+    var graph: DirectedGraph<Pair<String, String>, DefaultEdge>
+        get() = _graph
+        set(value) {
+            _graph = value
+        }
+
+    fun addTask(xmlNode: Node) {
+        if (xmlNode.hasAttributes()) {
+            val taskId = xmlNode.attributes.getNamedItem("id").nodeValue
+            val taskName = if (xmlNode.attributes.getNamedItem("name") != null) xmlNode.attributes.getNamedItem("name").nodeValue else ""
+            val vertexValues = Pair(taskId, taskName)
+            graphNodes.put(taskId, vertexValues)
+            graph.addVertex(vertexValues)
+        }
+    }
+
+    fun addGateway(xmlNode: Node) {
+        if (xmlNode.hasAttributes()) {
+            val gatewayId = xmlNode.attributes.getNamedItem("id").nodeValue
+            val gatewayName = if (xmlNode.attributes.getNamedItem("name") != null) xmlNode.attributes.getNamedItem("name").nodeValue else ""
+            val vertexValues = Pair(gatewayId, gatewayName)
+            graphNodes.put(gatewayId, vertexValues)
+            graph.addVertex(vertexValues)
+        }
+    }
+
+    fun addSequence(xmlNode: Node) {
+        if (xmlNode.hasAttributes()) {
+            if (xmlNode.attributes.item(1) == null || xmlNode.attributes.item(2) == null) {
+                return
+            }
+            val edgeSource = xmlNode.attributes.getNamedItem("sourceRef").nodeValue
+            val edgeDestination = xmlNode.attributes.getNamedItem("targetRef").nodeValue
+            val edgeSourceObject: Pair<String, String>? = graphNodes[edgeSource]
+            val edgeDestinationObject: Pair<String, String>? = graphNodes[edgeDestination]
+            graph.addEdge(edgeSourceObject, edgeDestinationObject)
+        }
+    }
+}
--- src/main/java/com/andreiruse/jdmengine/input/bpmnParser/BpmnParser.kt
+++ src/main/java/com/andreiruse/jdmengine/input/bpmnParser/BpmnParser.kt
@@ -1,15 +1,17 @@
 package com.andreiruse.jdmengine.input.bpmnParser
 
-import com.andreiruse.jdmengine.domain.Graph
+import com.andreiruse.jdmengine.domain.InMemoryGraph
+import org.w3c.dom.Node
 import org.xml.sax.SAXException
 import java.io.IOException
+import java.util.*
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.parsers.ParserConfigurationException
 
 class BpmnParser {
 
     @Throws(ParserConfigurationException::class, IOException::class, SAXException::class)
-    fun parse(bpmnFilePath: String): Graph {
+    fun parse(bpmnFilePath: String): InMemoryGraph {
         val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(bpmnFilePath)
         document.documentElement.normalize()
         val rootNode = document.documentElement
@@ -18,20 +20,25 @@ class BpmnParser {
                 .find { x -> "bpmn:process".equals(x?.nodeName) }
                 ?.childNodes
         if(bpmnNodes == null || bpmnNodes.length == 0) {
-            return Graph()
+            return InMemoryGraph()
         }
-        val graph = Graph()
+        val graph = InMemoryGraph()
         val bpmnNodeList = IntProgression.fromClosedRange(0, bpmnNodes.length, 1).map { x -> bpmnNodes.item(x) } //Fixme Length-1!
+        val edgesToProcessLater : MutableList<Node> = ArrayList()
         bpmnNodeList.forEach {
             if(it != null)
                 if("bpmn:sequenceFlow".equals(it.nodeName)) {
-                    graph.addSequence(it)
+                    edgesToProcessLater.add(it)
                 } else if("bpmn:exclusiveGateway".equals(it.nodeName)) {
                     graph.addGateway(it)
                 } else {
                     graph.addTask(it)
                 }
         }
+        for (node in edgesToProcessLater) {
+            graph.addSequence(node)
+        }
+        println("graph.graph = ${graph.graph}")
         return graph
     }
 
