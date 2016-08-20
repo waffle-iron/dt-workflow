98f0b2118fe068ed9ad4cb691110536d88bc888f
florinmocanu <florinmocanu91@yahoo.com>
2016-08-20 23:35:56 +0100

    9 create task domain object (#19)
    * Removed redundant classes in graph domain objects
    
    * Removed a bit of redundant code. Commented out some more code
    
    * Creaded base GraphTask.
    
    * Removed unused imports from GraphTask
    
    * Created base GraphTask.
    
    * Created base GraphTask.

--- src/main/java/com/docklandstech/workflow/domain/GraphTask.kt
+++ src/main/java/com/docklandstech/workflow/domain/GraphTask.kt
@@ -1,32 +1,13 @@
 package com.docklandstech.workflow.domain
 
 import com.docklandstech.workflow.domain.status.TaskStatus
-import org.apache.commons.lang3.builder.ToStringBuilder
-import org.w3c.dom.NamedNodeMap
-import org.w3c.dom.Node
-import org.w3c.dom.NodeList
 import java.util.*
 
-open class GraphTask(map: NamedNodeMap, nodeList: NodeList) : GraphObject {
-    private val taskId: Node
-    private val taskTitle: Node
+data class GraphTask(val taskId : String, val taskTitle: String) {
     private val status: TaskStatus = TaskStatus.CREATED
-    private val incomingLinkNames = ArrayList<String>()
-    private val outgoingLinkNames = ArrayList<String>()
+    private val outgoingLinkNames = ArrayList<GraphTask>()
 
-    init {
-        this.taskId = map.getNamedItem("id")
-        this.taskTitle = map.getNamedItem("name")
-        val edges: List<String> = IntProgression.fromClosedRange(0, nodeList.length, 1)
-                .map { index -> nodeList.item(index) }
-                .map { item -> (if ( item != null) item.nodeName else "") } //FixMe this will only get the tag name, not the value
-        edges.filter { nodeName -> "bpmn:incoming".equals(nodeName) }
-                .toCollection(incomingLinkNames)
-        edges.filter { nodeName -> "bpmn:outgoing".equals(nodeName) }
-                .toCollection(outgoingLinkNames)
-    }
-
-    override fun toString(): String {
-        return ToStringBuilder(this).append("taskId", taskId).append("taskTitle", taskTitle).append("status", status).append("incomingLinkNames", incomingLinkNames).append("outgoingLinkNames", outgoingLinkNames).toString()
+    fun addChild(nextElement: GraphTask) {
+        this.outgoingLinkNames.add(nextElement)
     }
 }
--- src/main/java/com/docklandstech/workflow/domain/UserTask.kt
+++
@@ -1,6 +0,0 @@
-package com.docklandstech.workflow.domain
-
-import org.w3c.dom.NamedNodeMap
-import org.w3c.dom.NodeList
-
-class UserTask(map: NamedNodeMap, nodeList: NodeList) : GraphTask(map, nodeList)
