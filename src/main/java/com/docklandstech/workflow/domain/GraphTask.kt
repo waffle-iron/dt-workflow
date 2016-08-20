000c6e094b673ab1a9acabd0b04aaf7bd6343670
Radu Chiorean <rchiorean@gmail.com>
2016-08-20 23:11:13 +0100

    Creaded base GraphTask.

--- src/main/java/com/docklandstech/workflow/domain/GraphTask.kt
+++ src/main/java/com/docklandstech/workflow/domain/GraphTask.kt
@@ -1,26 +1,28 @@
 package com.docklandstech.workflow.domain
 
+import com.docklandstech.workflow.domain.status.TaskStatus
+import org.apache.commons.lang3.builder.ToStringBuilder
 import org.w3c.dom.NamedNodeMap
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 import java.util.*
 
-open class GraphTask(map: NamedNodeMap, nodeList: NodeList) {
-    private val taskId: Node
-    private val taskTitle: Node
-//    private val status: TaskStatus = TaskStatus.CREATED
-    private val incomingLinkNames = ArrayList<String>()
-    private val outgoingLinkNames = ArrayList<String>()
+open class GraphTask(id : Int, name: String) {
+    private val taskTitle: String
+    private val taskId: Int
+    private val status: TaskStatus = TaskStatus.CREATED
+    private val outgoingLinkNames = ArrayList<GraphTask>()
 
     init {
-        this.taskId = map.getNamedItem("id")
-        this.taskTitle = map.getNamedItem("name")
-        val edges: List<String> = IntProgression.fromClosedRange(0, nodeList.length, 1)
-                .map { index -> nodeList.item(index) }
-                .map { item -> (if ( item != null) item.nodeName else "") } //FixMe this will only get the tag name, not the value
-        edges.filter { nodeName -> "bpmn:incoming".equals(nodeName) }
-                .toCollection(incomingLinkNames)
-        edges.filter { nodeName -> "bpmn:outgoing".equals(nodeName) }
-                .toCollection(outgoingLinkNames)
+        this.taskId = id
+        this.taskTitle = name
+    }
+
+    constructor(id: Int, name: String, nextElement: GraphTask ) : this(id, name){
+        this.addChild(nextElement)
+    }
+
+    fun addChild(nextElement: GraphTask) {
+        this.outgoingLinkNames.add(nextElement)
     }
 }
