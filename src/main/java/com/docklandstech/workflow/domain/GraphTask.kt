7387511e8384adaeadda1dd96eff42ba0415f382
Radu Chiorean <rchiorean@gmail.com>
2016-08-20 23:33:51 +0100

    Created base GraphTask.

--- src/main/java/com/docklandstech/workflow/domain/GraphTask.kt
+++ src/main/java/com/docklandstech/workflow/domain/GraphTask.kt
@@ -3,20 +3,9 @@ package com.docklandstech.workflow.domain
 import com.docklandstech.workflow.domain.status.TaskStatus
 import java.util.*
 
-open class GraphTask(id : String, name: String) {
-    private val taskTitle: String
-    private val taskId: String
+data class GraphTask(val taskId : String, val taskTitle: String) {
     private val status: TaskStatus = TaskStatus.CREATED
     private val outgoingLinkNames = ArrayList<GraphTask>()
-
-    init {
-        this.taskId = id
-        this.taskTitle = name
-    }
-
-    constructor(id: String, name: String, nextElement: GraphTask ) : this(id, name){
-        this.addChild(nextElement)
-    }
 
     fun addChild(nextElement: GraphTask) {
         this.outgoingLinkNames.add(nextElement)
