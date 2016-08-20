6bf389d791f943673eb54d2d732e57f385a4e9fe
Andrei Ruse <andrei@opengamma.com>
2016-08-20 21:35:02 +0100

    Add graph parser test with default BPMN graph

--- src/test/java/com/docklandstech/workflow/input/bpmnParser/BpmnParserTest.kt
+++ src/test/java/com/docklandstech/workflow/input/bpmnParser/BpmnParserTest.kt
@@ -1,10 +1,13 @@
 package com.docklandstech.workflow.input.bpmnParser
 
+import com.docklandstech.workflow.domain.InMemoryGraph
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.xml.sax.SAXParseException
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
+import java.util.*
 
 class BpmnParserTest {
     @Test(expected = SAXParseException::class)
@@ -22,5 +25,24 @@ class BpmnParserTest {
         Files.write(pathToFile, emptyBpmnXml.toByteArray(), StandardOpenOption.CREATE)
         val parser = BpmnParser()
         parser.parse(pathToFile.toString())
+    }
+
+    @Test
+    fun parseDefaultBpmnXml() {
+        val parser : BpmnParser = BpmnParser()
+        val graph : InMemoryGraph = parser.parse("src/main/resources/diagram.bpmn")
+        assertEquals(11, graph.getSize())
+        val expectedGraphElements = Arrays.asList("StartEvent_1",
+                "Task_1wwmb21",
+                "ExclusiveGateway_0dr5n4j",
+                "Task_00cjjzc",
+                "Task_0q3mwgo",
+                "EndEvent_0csk7l5",
+                "ExclusiveGateway_1k7wbt3",
+                "Task_14m4ix9",
+                "Task_10zg8wa",
+                "ExclusiveGateway_0jg3gwp",
+                "EndEvent_0j4fie2")
+        assert(graph.getVertices().map { x -> x.first }.containsAll(expectedGraphElements))
     }
 }
